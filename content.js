193dbf5e4c691359c8d43a01172e23d191abef02
MitaliC21 <71689270+MitaliC21@users.noreply.github.com>
2021-08-31 16:03:22 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,19 +1,65 @@
+let arr = [];
+let information = [];
+
 chrome.runtime.onMessage.addListener(gotMessage)
 
 function gotMessage(request, sender, sendResponse){
   if(request.data === "pass"){
     let p = document.querySelectorAll("[dir = 'ltr']");
-    for(let i=1;i<p.length;i++){
-        console.log(p[i].innerText)
-    }
+    
+    for(let i=1;i<= 10;i++){
+        console.log(p[i].firstChild.innerText)
+        arr.push(p[i].parentElement.href)
+        information.push(p[i].firstChild.innerText)
+      }
+      sendResponse({arr, information});
    }
+  
+   if(request.about === 'about'){
+     sendResponse(content());
+   }
+  
    if(request.msg === "alert"){
+     console.clear()
+     for(const page of request.result){
+       for(let i=0; i<10; i++){
+         console.log(page[i]);
+       }
+     }
+     console.log("**********Information stored in array data structure**********")
+     console.log(request.pInfo)
+     console.log("**********Expanded view of information**********")
+     for(const inf of request.pInfo ){
+      console.log(inf);
+     }
+   
      alert("Done");
+     
    }
 }
 
 
- 
-        
+function content(){
+  let profile = {}
+  profile.name = document.querySelector('.text-heading-xlarge').innerText;
+
+  if(document.querySelector('.pv-about-section')){
+  profile.about = document.querySelector('.pv-about-section').innerText;
+  }
+  else{console.log("Information not available")}
+
+  profile.connections = document.querySelector('.t-bold').innerText;
+
+ if(document.querySelectorAll('#experience-section')){
+  profile.experience = [];
+  let info = document.querySelectorAll('.pv-entity__secondary-title.separator');
+  for(let e of info ){
+    profile.experience.push(e.parentElement.innerText);
+  }
+  }
+  else{console.log("Information not available")}
+  console.log(profile);
+  return profile;
+ }
 
   
--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 let sPage = document.querySelector('#start');
 let ePage = document.querySelector('#end');
 const button = document.getElementById('button');
-
+let finalArr = [];
+let profile = [];
 
 function goToUrl(tab, url) {
   chrome.tabs.update(tab.id, { url });
   return new Promise((resolve) => {
     chrome.tabs.onUpdated.addListener(function onUpdated(tabId, info) {
-      console.log(info.status);
       if (info.status === "complete") {
         chrome.tabs.onUpdated.removeListener(onUpdated);
         resolve();
@@ -16,25 +16,43 @@ function goToUrl(tab, url) {
   });
 }
 
+function goToLinks(tab){
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tab, {"data": 'pass'}, async function(response){
+      finalArr.push(response.information);
+      console.log(response);
+      for(let i=0;i<10;i++){
+        await goToUrl(tab, response.arr[i])
+        chrome.tabs.sendMessage(tab, {"about": 'about'}, function(res){
+          profile.push(res);
+        });
+      }
+      
+      resolve();
+      });
+  })
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   button.addEventListener("click", async (e)=> {
     e.preventDefault();
-
+    button.disabled=true;
     let pageStart = sPage.value;
     let pageEnd = ePage.value;
-   console.log(pageStart)
-   console.log(pageEnd)
     chrome.tabs.query({ currentWindow: true, active: true }, async (tabs) => {
       var activeTab = tabs[0];
       var count = pageEnd-pageStart;
-     for(count; count > 0; count--){
-       await goToUrl(activeTab.id, `https://www.linkedin.com/search/results/people/?keywords=data%20science&origin=SWITCH_SEARCH_VERTICAL&page=${++pageStart}&sid=uf%2C`)
-
-       chrome.tabs.sendMessage(activeTab.id, {"data": 'pass'});
-       console.log("solved")
-        }
-        chrome.tabs.sendMessage(activeTab.id, {"msg": 'alert'});
-
+      await goToUrl(activeTab.id, `https://www.linkedin.com/search/results/people/?keywords=data%20science&origin=SWITCH_SEARCH_VERTICAL&page=${pageStart}&sid=uf%2C`)
+     
+     for(count; count >= 0; count--){
+      await goToLinks(activeTab.id);
+      
+      await goToUrl(activeTab.id, `https://www.linkedin.com/search/results/people/?keywords=data%20science&origin=SWITCH_SEARCH_VERTICAL&page=${++pageStart}&sid=uf%2C`)
+       
+      }
+       
+      chrome.tabs.sendMessage(activeTab.id, {"msg": 'alert', "result": finalArr, "pInfo": profile});
+         
     });
 });
 });
